0270a777d4f7756f0b36c1a7ecb1df7a9de747ae
Brian Hilnbrand <brhiln@gmail.com>
2018-06-21 21:37:03 -0700

    Add: LeNet implementation

--- train.py
+++ train.py
@@ -38,16 +38,39 @@ y_train = np.array(augmented_measurements)
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Convolution2D, Lambda
 
+# model = Sequential()
+# # add a 3x3 convolution on top, with 32 output filters:
+# # model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(160,320,3)))
+# # model.add(Convolution2D(32, 3, 3))
+# model.add(Lambda(lambda x : x / 255.0 - 0.5, input_shape=(160,320,3)))
+# model.add(Convolution2D(6, 5, 5, activation='relu'))
+# model.add(Flatten())
+# # model.add(Flatten(input_shape=(160,320,3)))
+# # model.add(Dense(100))
+# model.add(Dense(1))
+
 model = Sequential()
-# add a 3x3 convolution on top, with 32 output filters:
-# model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(160,320,3)))
-# model.add(Convolution2D(32, 3, 3))
-model.add(Lambda(lambda x : x / 255.0 - 0.5, input_shape=(160,320,3)))
-model.add(Convolution2D(6, 5, 5, activation='relu'))
+# For an explanation on conv layers see http://cs231n.github.io/convolutional-networks/#conv
+# By default the stride/subsample is 1 and there is no zero-padding.
+# If you want zero-padding add a ZeroPadding layer or, if stride is 1 use border_mode="same"
+model.add(Convolution2D(12, 5, 5, activation = 'relu', input_shape=in_shape, init='he_normal'))
+
+# For an explanation on pooling layers see http://cs231n.github.io/convolutional-networks/#pool
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(25, 5, 5, activation = 'relu', init='he_normal'))
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Flatten the 3D output to 1D tensor for a fully connected layer to accept the input
 model.add(Flatten())
-# model.add(Flatten(input_shape=(160,320,3)))
-# model.add(Dense(100))
-model.add(Dense(1))
+model.add(Dense(180, activation = 'relu', init='he_normal'))
+model.add(Dropout(0.5))
+model.add(Dense(100, activation = 'relu', init='he_normal'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation = 'softmax', init='he_normal')) #Last layer with one output per class
+
+
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs)
