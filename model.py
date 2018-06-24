f5176fd7fe4722c7b6d9ef6b2b30f24140a37483
Brian Hilnbrand <brhiln@gmail.com>
2018-06-24 12:35:48 -0700

    comments, save history as image

--- model.py
+++ model.py
@@ -3,20 +3,25 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
+# command line flags
 flags = tf.app.flags
 FLAGS = flags.FLAGS
-
 flags.DEFINE_integer('epochs', 50, "The number of epochs.")
 flags.DEFINE_string('folder', './', "The folder.")
+
+# read driving log from sim
 lines = []
 with open(FLAGS.folder + '/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
 
+# load all images from sim and corresponding measurements. save in parallel lists
 images = []
 measurements = []
 for line in lines:
+
+	# center images
 	source_path = line[0]
 	filename = source_path.split('/')[-1]
 	current_path = FLAGS.folder + '/IMG/' + filename
@@ -25,8 +30,10 @@ for line in lines:
 	measurement = float(line[3])
 	measurements.append(measurement)
 
+	# steering correction for side images
 	correction = 0.2
 
+	# left images, adjust steering back to center of road
 	source_path = line[1]
 	filename = source_path.split('/')[-1]
 	current_path = FLAGS.folder + '/IMG/' + filename
@@ -34,6 +41,7 @@ for line in lines:
 	images.append(image)
 	measurements.append(measurement+correction)
 
+	# right images, adjust steering back to center of road
 	source_path = line[2]
 	filename = source_path.split('/')[-1]
 	current_path = FLAGS.folder + '/IMG/' + filename
@@ -41,8 +49,7 @@ for line in lines:
 	images.append(image)
 	measurements.append(measurement-correction)
 
-print ("IMAGES:", len(images))
-
+# flip images and corresponding measurements to generate twice as much traning data
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
 	augmented_images.append(image)
@@ -50,22 +57,24 @@ for image, measurement in zip(images, measurements):
 	augmented_images.append(cv2.flip(image,1))
 	augmented_measurements.append(measurement*-1.0)
 
-print ("AUGMENTED:", len(augmented_images))
-
+# store data in numpy array
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
+# import libraries from keras
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Convolution2D, Lambda, MaxPooling2D, Cropping2D
 
+# static definitions
 input_shape=(160,320,3)
 num_classes=1
 
+# setup Keras model, normalize, and crop
 model = Sequential()
 model.add(Lambda(lambda x : x / 255.0 - 0.5, input_shape=input_shape))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 
-# LeNet
+# LeNet model
 # model.add(Convolution2D(6,5,5,activation='relu'))
 # model.add(MaxPooling2D())
 # model.add(Convolution2D(6,5, 5, activation='relu'))
@@ -75,7 +84,7 @@ model.add(Cropping2D(cropping=((70,25),(0,0))))
 # model.add(Dense(84))
 # model.add(Dense(num_classes))
 
-#Nvidia
+# Nvidia model
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
@@ -87,10 +96,11 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(num_classes))
 
+# compile model, train, and save the training history
 model.compile(loss='mse', optimizer='adam')
 history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs)
 
-model.save('bmodel.h5')
+model.save('model.h5')
 
 ### print the keys contained in the history object
 print(history_object.history.keys())
@@ -103,3 +113,4 @@ plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
 plt.show()
+savefig('model.png')
